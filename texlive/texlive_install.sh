89be7927521a9078b9732538a4c9168ab3c90e23
Maximilian Starke <maximilian.starke@medionmail.com>
2021-11-26 04:28:46 +0100

    build Latex script on Azure (#18)
    * upload some azure script
    
    * fix
    
    * test
    
    * test
    
    * execution rights
    
    * run latex
    
    * try reload env variables
    
    * check path
    
    * test this
    
    * check export behavior in azure steps
    
    * try bash
    
    * try this
    
    * try this
    
    * try this
    
    * wrong position in script
    
    * try this
    
    * try fix yml
    
    * try install biber later
    
    * try fix
    
    * Update azure-pipelines.yml
    
    * try to trigger build
    
    * try trigger build
    
    * try trigger
    
    * Update azure-pipelines.yml
    
    * add deploy script
    
    * check if still working
    
    * add execution rights
    
    * tidy up
    
    * check if working
    
    * fix azure yml
    
    * typo fix
    
    * try this as a fix
    
    * try the other way round

--- texlive/texlive_install.sh
+++ texlive/texlive_install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # Originally from https://github.com/latex3/latex3
 
@@ -8,6 +8,7 @@
 
 # See if there is a cached version of TL available
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
+echo "PATH=/tmp/texlive/bin/x86_64-linux:$PATH" >> ~/.bashrc
 if ! command -v texlua > /dev/null; then
   # Obtain TeX Live
   wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
