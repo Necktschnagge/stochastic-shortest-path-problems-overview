c27c48faaece301ba1ae002d5cef2a7b34492e2c
Maximilian Starke <maximilian.starke@medionmail.com>
2022-09-18 12:19:00 +0200

    echos

--- script/conditional_deploy_artifacts.sh
+++ script/conditional_deploy_artifacts.sh
@@ -41,9 +41,15 @@ fi
 
 #environment variables:
 echo "Debug: Show git log history visually:"
+echo git remote
+git remote
+echo git fetch https://${git_username}:${git_access_token}@github.com/${user_repo_id} #--deepen=4 #Azure Pipelines per default fetches using '--depth=1'
 git fetch https://${git_username}:${git_access_token}@github.com/${user_repo_id} #--deepen=4 #Azure Pipelines per default fetches using '--depth=1'
+echo git fetch https://${git_username}:${git_access_token}@github.com/${user_repo_id} --deepen=4 #Azure Pipelines per default fetches using '--depth=1'
 git fetch https://${git_username}:${git_access_token}@github.com/${user_repo_id} --deepen=4 #Azure Pipelines per default fetches using '--depth=1'
+echo git log --graph --oneline --all
 git log --graph --oneline --all
+echo git branch --all
 git branch --all
 echo "Determine last git commit on feature branch:"
 git_hash_last_commit=$(git rev-parse HEAD^2) # CI merges the current feature branch commit into pull request base branch. Therefore on CI there is always one commit more. -> use ^2 for merge ancestor (see https://medium.com/@gabicle/git-ancestry-references-explained-bd3a84a0b821)
